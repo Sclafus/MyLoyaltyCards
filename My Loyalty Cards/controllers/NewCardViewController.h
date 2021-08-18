093455c303e7c0d2b405e1b4b758749c4720c736
Sclafus <alessandro.sclafani00@gmail.com>
2021-08-18 19:27:33 +0200

    added view controller for new card view

---
+++ My Loyalty Cards/controllers/NewCardViewController.h
@@ -0,0 +1,28 @@
+//
+//  NewCardViewController.h
+//  My Loyalty Cards
+//
+//  Created by Alessandro Sclafani on 18/08/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewCardViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *companyNameTextField;
+
+@property (strong, nonatomic) IBOutlet UITextField *clientIdTextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *cardTypeSegmentOutput;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *cardColorSegmentedOutput;
+
+- (IBAction)cardColorAction:(id)sender;
+
+- (IBAction)doneButtonAction:(id)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ My Loyalty Cards/controllers/NewCardViewController.m
@@ -0,0 +1,75 @@
+//
+//  NewCardViewController.m
+//  My Loyalty Cards
+//
+//  Created by Alessandro Sclafani on 18/08/2021.
+//
+
+#import "NewCardViewController.h"
+#import "Card.h"
+#import "ColorHelper.h"
+
+@interface NewCardViewController ()
+
+@end
+
+@implementation NewCardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)doneButtonAction:(id)sender {
+    NSString *companyName = self.companyNameTextField.text;
+    NSString *clientId =  self.clientIdTextField.text;
+    bool barcode = self.cardTypeSegmentOutput.selectedSegmentIndex == 1 ? true : false;
+    NSString *colorHex = [ColorHelper stringFromColor:self.cardColorSegmentedOutput.selectedSegmentTintColor];
+    NSLog(@"%@ %@ %d %@", clientId, companyName, barcode, colorHex);
+    
+    Card *newCard = [[Card alloc] initWithCompanyName:companyName
+                                     stringId:clientId
+                                     colorHex:colorHex
+                                    isBarcode:barcode];
+    // TODO add the card to the list
+    return;
+}
+
+- (IBAction)cardColorAction:(id)sender {
+    
+    switch (self.cardColorSegmentedOutput.selectedSegmentIndex) {
+        case 0:
+            self.cardColorSegmentedOutput.selectedSegmentTintColor = [UIColor systemBlueColor];
+            break;
+            
+        case 1:
+            self.cardColorSegmentedOutput.selectedSegmentTintColor = [UIColor systemRedColor];
+            break;
+        
+        case 2:
+            self.cardColorSegmentedOutput.selectedSegmentTintColor = [UIColor systemGreenColor];
+            break;
+            
+        case 3:
+            self.cardColorSegmentedOutput.selectedSegmentTintColor = [UIColor systemYellowColor];
+            break;
+            
+        case 4:
+            self.cardColorSegmentedOutput.selectedSegmentTintColor = [UIColor systemPurpleColor];
+            break;
+            
+        default:
+            break;
+    }
+}
+@end
